9b4c774f0c222dde912d46551e03c5d348190bcb
ritikagarwal26 <ritikagarwal535@gmail.com>
2022-08-26 16:24:16 +0530

    installed mongoose and connected to database User

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var app = express
 const http = require('http').Server(app)
 const mongoose = require('mongoose')
 
+var dburl = 'mongodb://127.0.0.1:27017/User'
 
+
+mongoose.connect(dburl, (err) => {
+    console.log('db connected', err)
+})
 
 var Server = http.listen(3000, ()=>{
     console.log(`Server is running on port:`, Server.address().port)
